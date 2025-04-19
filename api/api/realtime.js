4ff32d5ed0c28a01ee284b98ac84e9103d3ced03
Miguel Sanchez-Grice <msanchezgrice@gmail.com>
2025-04-19 17:53:21 -0500

    Simplify Edge function for Vercel deployment

--- api/api/realtime.js
+++ api/api/realtime.js
@@ -1,14 +1,12 @@
-// Simple Edge API handler
 export const config = {
   runtime: 'edge'
 };
 
 export default function handler(request) {
-  // For non-WebSocket requests, return a simple message
   return new Response("WebSocket endpoint for talking objects audio API", {
     status: 200,
     headers: {
       "Content-Type": "text/plain"
     }
   });
-}
+} 
