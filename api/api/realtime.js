6d0136d3713269f908cbe5bc0b3a1d8c0dbacffd
Miguel Sanchez-Grice <msanchezgrice@gmail.com>
2025-04-19 17:55:01 -0500

    Replace Edge function with standard serverless function

--- api/api/realtime.js
+++ api/api/realtime.js
@@ -1,12 +1,4 @@
-export const config = {
-  runtime: 'edge'
-};
-
-export default function handler(request) {
-  return new Response("WebSocket endpoint for talking objects audio API", {
-    status: 200,
-    headers: {
-      "Content-Type": "text/plain"
-    }
-  });
-} 
+// Standard serverless function for Vercel deployment
+module.exports = (req, res) => {
+  res.status(200).send('WebSocket endpoint for talking objects audio API');
+}; 
